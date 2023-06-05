f0d2d30c1719ba4393f648b1e117885ae7023b01
Tuan Le <leminhtuan169@gmail.com>
2023-06-05 12:47:32 +0700

    todo app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,125 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
+// const photoCards = ref([
 
-const count = ref(0)
+// ])
 </script>
 
 <template>
-    <div class="card">
-        <button type="button" @click="count++">count is {{ count }} s</button>
-        <p>
-            Edit
-            <code>components/HelloWorld.vue</code> to test HMR
-        </p>
+    <div class="photo-card">
+        <div class="photo-card__header">
+            <div class="flex space-between container">
+                <div class="photo-card__name flex">
+                    <img src="../assets/vue.svg" alt="" />
+                    <p>Hieu Tran</p>
+                </div>
+                <div class="photo-card__button">
+                    <button>Indie Game</button>
+                    <button>Entry Game</button>
+                    <button>Community</button>
+                </div>
+            </div>
+        </div>
+        <div class="photo-card__main">
+            <div class="container">
+                <h2>Photo Cards:</h2>
+                <div class="flex wrap photo-card__content">
+                    <div v-for="arr in 18" :key="arr" class="photo-card__block">
+                        <div class="photo-card__img">
+                            <img src="/src/assets/img.jpg" alt="" />
+                        </div>
+                        <p>this is a long test</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-            >create-vue</a
-        >, the official Vue + Vite starter
-    </p>
-    <p>
-        Install
-        <a href="https://github.com/johnsoncodehk/volar" target="_blank"
-            >Volar</a
-        >
-        in your IDE for a better DX
-    </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-    color: #888;
+<style lang="scss" scoped>
+// var
+$primary-clg: rgb(104, 200, 209);
+// global
+p {
+    color: #fff;
+}
+.flex {
+    display: flex;
+}
+.space-between {
+    justify-content: space-between;
+}
+.wrap {
+    flex-wrap: wrap;
+}
+.container {
+    margin: 0 20%;
+}
+button {
+    border-radius: 2px;
+    border: 1px solid #ffff;
+    padding: 0.6em 1.2em;
+    transition: border-color 0.25s;
+    margin: 20px auto;
+    color: #ffff;
+    background: $primary-clg;
+}
+// primary
+.photo-card {
+    &__header {
+        background-color: $primary-clg;
+        @media screen and (max-width: 768px) {
+            flex-direction: column;
+        }
+    }
+    &__button {
+        button:nth-child(-n + 2) {
+            margin-right: 12px;
+        }
+    }
+    &__name {
+        align-self: center;
+        p {
+            margin: auto;
+        }
+        img {
+            width: 30px;
+            height: 30px;
+        }
+    }
+    &__block {
+        width: calc(20% - 20px);
+        margin: 0 10px;
+        border-radius: 2px;
+        p {
+            margin: 12px 0;
+            color: #000;
+        }
+        @media screen and (max-width: 768px) {
+            width: 100%;
+        }
+    }
+    &__content {
+        margin: 0 -10px;
+        @media screen and (max-width: 768px) {
+            flex-direction: column;
+        }
+    }
+    &__img {
+        position: relative;
+        display: block;
+        padding-top: 60%;
+        img {
+            // display: block;
+            // max-width: 100%;
+            // height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
 }
 </style>
