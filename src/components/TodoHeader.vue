2a5a167becc013b0d0e381697d129f2d61c22972
Tuan Le <leminhtuan169@gmail.com>
2023-06-07 10:18:29 +0700

    :zap: fix router

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import HcDropdown from "../components/global/TodoDropdown.vue";
-const isNew = ref<number>(0);
 </script>
 
 <template>
@@ -10,21 +8,6 @@ const isNew = ref<number>(0);
       <p class="header__content">SunFlower</p>
       <img src="../assets/images.png" alt="img" class="header__icon" />
       <p class="header__content">Broccoli</p>
-      <div style="align-self: center">
-        <hc-dropdown>
-          <template #dropdown-toggle>
-            <div class="header__menu__toggle" />
-          </template>
-          <template #dropdown-items>
-            <a class="hc-ma-sm" href="#">自分の記録</a>
-            <a class="hc-ma-sm" href="#">体重グラフ</a>
-            <a class="hc-ma-sm" href="#">目標</a>
-            <a class="hc-ma-sm" href="#">選択中のコース</a>
-            <a class="hc-ma-sm" href="#">コラム一覧</a>
-            <a class="hc-ma-sm" href="#">設定</a>
-          </template>
-        </hc-dropdown>
-      </div>
     </div>
   </div>
 </template>
@@ -35,7 +18,7 @@ const isNew = ref<number>(0);
 .header {
   &__icon {
     flex-grow: 1;
-    height: 85px;
+    height: 82px;
     object-fit: cover;
   }
   &__content {
