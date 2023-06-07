7c123dfb1dda2059f6a5493d25a97ef24a72c832
Tuan Le <leminhtuan169@gmail.com>
2023-06-07 10:55:47 +0700

    :ambulance: test

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Homepage from "../views/homepage/";
-console.log(import.meta.env.BASE_URL);
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [...Homepage],
