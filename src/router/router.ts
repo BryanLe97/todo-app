9909a8faf45f148e0da7d83338e31828b6e037d2
Tuan Le <leminhtuan169@gmail.com>
2023-06-07 11:00:44 +0700

    :ambulance: test

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Homepage from "../views/homepage/";
 export default createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/todo-app/"),
   routes: [...Homepage],
 });
